d3f002f440a29efc12e52f1c972bf0c084831fe9
arpruss <arpruss@gmail.com>
2020-12-12 09:50:29 -0600

    foo

---
+++ fetchtest.js
@@ -0,0 +1,16 @@
+class ScratchFetch {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "Fetch",
+            "name": "Fetch",
+            "blocks": [ /* add later */ ]
+        }
+    }
+
+    /* add methods for blocks */
+}
+
+Scratch.extensions.register(new ScratchFetch())
